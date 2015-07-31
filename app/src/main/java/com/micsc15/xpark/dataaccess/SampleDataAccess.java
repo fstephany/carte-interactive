49f34e6dd9ef6331d3be22679f3ce229ae974b23
Valentin Taleb <Valentin@Valentins-MacBook-Pro.local>
2015-07-31 11:32:49 +0200

    Prepare the project for newbies :)

---
+++ app/src/main/java/com/micsc15/xpark/dataaccess/SampleDataAccess.java
@@ -0,0 +1,19 @@
+package com.micsc15.xpark.dataaccess;
+
+public class SampleDataAccess {
+
+    // -------------- Objects, Variables -------------- //
+
+
+    // ------------------ Properties ------------------ //
+
+
+    // ------------------- Methods -------------------- //
+
+
+    // ------------- DataBase Management -------------- //
+
+
+    // ----------------- Miscellaneous ---------------- //
+
+}
---
+++ app/src/main/java/com/micsc15/xpark/managers/SampleManager.java
@@ -0,0 +1,16 @@
+package com.micsc15.xpark.managers;
+
+public class SampleManager {
+
+    // -------------- Objects, Variables -------------- //
+
+
+    // --------------- Public Methods ----------------- //
+
+
+    // --------------- Private Methods ---------------- //
+
+
+    // ----------------- Miscellaneous ---------------- //
+
+}
