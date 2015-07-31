fbb0c83a888531b85d8ca48fbb1d117b1fdb00d9
Valentin Taleb <Valentin@Valentins-MacBook-Pro.local>
2015-07-31 14:10:24 +0200

    no message

---
+++ app/src/main/java/com/micsc15/xpark/activities/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.micsc15.xpark.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    // -------------- Objects, Variables -------------- //
+
+
+    // --------------------- Views -------------------- //
+
+
+    // ------------------ LifeCycle ------------------- //
+
+
+    // ------------------ Listeners ------------------- //
+
+
+    // ----------------- GUI Adapter ------------------ //
+
+
+    // ----------------- Miscellaneous ---------------- //
+
+}
