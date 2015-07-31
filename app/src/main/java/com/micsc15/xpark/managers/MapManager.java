aefac6ebe02f083f6984a03004c07de2aee8dbeb
Matthieu Vandenhende <vandenhende.matthieu@gmail.com>
2015-07-31 17:23:49 +0200

    first check in

---
+++ app/src/main/java/com/micsc15/xpark/managers/MapManager.java
@@ -0,0 +1,7 @@
+package com.micsc15.xpark.managers;
+
+/**
+ * Created by Matthieu on 31-07-15.
+ */
+public class MapManager {
+}
---
+++ app/src/main/java/com/micsc15/xpark/models/maps/Card.java
@@ -0,0 +1,7 @@
+package com.micsc15.xpark.models.maps;
+
+/**
+ * Created by Matthieu on 31-07-15.
+ */
+public class Card {
+}
