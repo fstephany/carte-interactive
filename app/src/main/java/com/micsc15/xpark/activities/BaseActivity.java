2ccf87a439f0ed293c92e1b5800761347c1eb18e
Matthieu Vandenhende <vandenhende.matthieu@gmail.com>
2015-08-02 15:29:55 +0200

    no message

--- app/src/main/java/com/micsc15/xpark/activities/BaseActivity.java
+++ app/src/main/java/com/micsc15/xpark/activities/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.micsc15.xpark.activities;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -8,10 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.micsc15.xpark.R;
+
+import java.util.ArrayList;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -54,9 +61,11 @@ public class BaseActivity extends AppCompatActivity {
         drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name, R.string.app_name) {
 
             public void onDrawerOpened(View drawerView) {
+
             }
 
             public void onDrawerClosed(View view) {
+
             }
         };
         drawerToggle.setDrawerIndicatorEnabled(true);
@@ -67,8 +76,32 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     private void manageNavigationDrawerContent(){
+        ArrayList<NavigationMenuItem> menuitems = new ArrayList<NavigationMenuItem>();
+
+        menuitems.add(new NavigationMenuItem(R.drawable.map_drawer, "Carte"));
+        menuitems.add(new NavigationMenuItem(R.drawable.list_drawer, "liste des activités"));
+        menuitems.add(new NavigationMenuItem(R.drawable.news_drawer, "Actualité"));
+        menuitems.add(new NavigationMenuItem(R.drawable.info_drawer, "Infos pratique"));
+        menuitems.add(new NavigationMenuItem(R.drawable.mail_drawer, "Contact"));
+
         ListView listView = (ListView) findViewById(R.id.listView_NavigationDrawer);
-        listView.setAdapter(new Adapter());
+        ImageView imageview =  new ImageView(getBaseContext());
+        imageview.setImageResource(R.drawable.drawer_image);
+
+
+        listView.addHeaderView(imageview);
+        listView.setAdapter(new Adapter(menuitems));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              // if(position == 3){
+                //   startActivity(new Intent(BaseActivity.this, NewsActivity.class));
+               //}else{
+                //   startActivity(new Intent(BaseActivity.this, MapActivity.class));
+               //}
+            }
+        });
     }
 
 
@@ -76,7 +109,7 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item))
+         if (drawerToggle.onOptionsItemSelected(item))
             return true;
 
         return super.onOptionsItemSelected(item);
@@ -87,14 +120,20 @@ public class BaseActivity extends AppCompatActivity {
 
     private class Adapter extends BaseAdapter {
 
-        @Override
-        public int getCount() {
-            return 0;
+        public ArrayList<NavigationMenuItem> menuItems;
+
+        public Adapter(ArrayList<NavigationMenuItem> menuItems){
+            this.menuItems = menuItems;
         }
 
         @Override
-        public Object getItem(int position) {
-            return null;
+        public int getCount() {
+            return menuItems.size();
+        }
+
+        @Override
+        public NavigationMenuItem getItem(int position) {
+            return menuItems.get(position);
         }
 
         @Override
@@ -104,11 +143,29 @@ public class BaseActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            NavigationMenuItem item = getItem(position);
+
+            View view = getLayoutInflater().inflate(R.layout.menu_item, null);
+            ((TextView)view.findViewById(R.id.textView)).setText(item.text);
+            ((ImageView) view.findViewById(R.id.imageView)).setImageResource(item.imageResourceID);
+
+            return view;
         }
+
     }
 
 
     // ----------------- Miscellaneous ---------------- //
+    public class NavigationMenuItem{
 
+        public int imageResourceID;
+        public String text;
+
+        public NavigationMenuItem(int imageResourceID, String text){
+            this.imageResourceID = imageResourceID;
+            this.text = text;
+        }
+    }
 }
+
+
