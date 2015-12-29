6fa0bfe6672dba5f1e90511e170cd61be8ec2609
Francois Stephany <tulipe.moutarde@gmail.com>
2015-12-29 11:25:00 +0100

    Add Crashlytics

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# README
+
+Fork de l'app Carte Interactive développée par une team au MIC Summer Camp.
+
+# Travis CI
+
+![](https://api.travis-ci.org/fstephany/carte-interactive.svg?branch=master)
--- app/src/main/java/com/micsc15/xpark/activities/SplashActivity.java
+++ app/src/main/java/com/micsc15/xpark/activities/SplashActivity.java
@@ -2,6 +2,12 @@ package com.micsc15.xpark.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends BaseActivity {
 
@@ -18,7 +24,7 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Fabric.with(this, new Crashlytics());
         startActivity(new Intent(SplashActivity.this, MapActivity.class));
         finish();
     }
